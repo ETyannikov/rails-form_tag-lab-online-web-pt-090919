c0c12479c339070742efc48f385ad0cc56a646a5
Eddy Tyannikov <etyannikov@gmail.com>
2020-02-09 23:33:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,16 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+    params
+  end
+  
+  def create
+        @student = Student.new(student_params(:first_name, :last_name))
+        @student.save
+        redirect_to student_path(@student)
+    end
+
 
 end
